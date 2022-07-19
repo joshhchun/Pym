cb3a3edfc017943393b557ec6638460d12f0ed63
joshhchun <jchun2@nd.edu>
2022-07-19 19:06:58 +0000

    security update

--- pym-api/routes/image.js
+++ pym-api/routes/image.js
@@ -3,13 +3,26 @@ const router = express.Router({ mergeParams: true });
 const Post = require("../models/Post");
 const fs = require("fs");
 
+var options = {
+  dotfiles: 'deny'
+}
 // API Endpoint to retrieve post data from the database
 router.get("/:id", async (req, res) => {
   try {
     const post = await Post.findOne({ shortId: req.params.id });
-    // res.set('Content-type', 'image/jpeg');
-    res.sendFile("/usr/src/app/" + post.value);
+    if (post.group === "image") {
+	if (post.value.includes(process.env.FORBIDDEN)) return res.send("tsk tsk tsk tsk");
+    	res.sendFile(process.env.URL + post.value, options, function (err) {
+	  if (err) {
+	    console.log(err);
+	    res.send(null);
+	  }
+	});
+     } else {
+	res.send("tsk tsk tsk tsk");
+     }
   } catch (e) {
+    console.log(e);
     console.log("No Post with that ID!");
     res.json(null)
   }
